4289c84f210a1cf2a892c32c2a3b6a2f7d913c14
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-02-12 16:36:01 +0500

    add metadata and category to posts table

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,8 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Schema::defaultStringLength(255);
+        Schema::defaultStringLength(255);
         Paginator::useBootstrap();
     }
-    
 }
